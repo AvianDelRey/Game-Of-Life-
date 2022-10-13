cbf18b2647fd0a7480204862586216198ee8c08a
Roy Haynes <Roy@art4thereal.com>
2022-10-12 20:12:37 -0400

    Feature: Make Grid Method
    Co-authored-by: Felix

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
-let cols;
-let rows;
+// let cols;
+// let rows;
+// let grid;
 let grid;
+let gridold;
+let gridmax;
 
+function initGrid() {
+  gridmax = 100;
+  grid = new Array(gridmax);
+  for (let i = 0; i < gridmax; i++) {
+    grid[i] = new Array(gridmax);
+    for (let j = 0; j < gridmax; j++) {
+      const randomLife = Math.random() > .5 ? 0 : 1;
+      grid[i][j] = randomLife;
+    }
+  }
+  return grid;
+}
+
+console.table(initGrid())
 
 //function to make 2d array but there is nothing inside of them
 
-function fillArr(cols, rows) {
-  rows = 5
-  cols = 5
-  let arr = new Array(cols);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = new Array(rows);
-    console.log(arr[i]);
-    return arr
-  }
-}
+// function fillArr(cols, rows) {
+//   rows = 5
+//   cols = 5
+//   let arr = new Array(cols);
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] = new Array(rows);
+//     console.log(arr[i]);
+//     return arr
+//   }
+// }
 
-//this should fill each cell in the array with either a 1 or a 0
-// having a problem filling each cell here 
+// //this should fill each cell in the array with either a 1 or a 0
+// // having a problem filling each cell here 
 
-function setup() {
-  grid = fillArr(cols, rows)
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[i][j] = grid.fill(math.floor(Math.random(2)))
+// function setup() {
+//   grid = fillArr(cols, rows)
+//   for (let i = 0; i < cols; i++) {
+//     for (let j = 0; j < rows; j++) {
+//       grid[i][j] = grid.fill(math.floor(Math.random(2)))
       
-    }
-  }
-  return grid
-}
+//     }
+//   }
+//   return grid
+// }
