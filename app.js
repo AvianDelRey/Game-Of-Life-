47e579083d652504a30f95c98f14fbc5e6c6e79e
Roy Haynes <Roy@art4thereal.com>
2022-10-25 14:13:59 -0400

    able to count neighbor cells

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// let cols;
-// let rows;
-// let grid;
 let grid;
 let gridold;
 let gridmax;
@@ -11,38 +8,65 @@ function initGrid() {
   for (let i = 0; i < gridmax; i++) {
     grid[i] = new Array(gridmax);
     for (let j = 0; j < gridmax; j++) {
-      const randomLife = Math.random() > .5 ? 0 : 1;
-      grid[i][j] = randomLife;
+      const cells = Math.random() > .5 ? 0 : 1;
+      grid[i][j] = cells;
     }
   }
   return grid;
 }
 
-console.table(initGrid())
+grid = initGrid()
 
-//function to make 2d array but there is nothing inside of them
+//iterate through grid
 
-// function fillArr(cols, rows) {
-//   rows = 5
-//   cols = 5
-//   let arr = new Array(cols);
-//   for (let i = 0; i < arr.length; i++) {
-//     arr[i] = new Array(rows);
-//     console.log(arr[i]);
-//     return arr
-//   }
-// }
+function iterateGrid() {
+  // console.log(`grid => ${grid}`)
+  // console.log(`grid. length => ${grid.length }`)
+  for (let i = 0; i < grid.length; i++) {
+    let cols = grid[i]
+     for (let j = 0; j < cols.length; j++) {
+        let cell = grid[i][j];
+      // console.log(`cells => ${cell}`)
 
-// //this should fill each cell in the array with either a 1 or a 0
-// // having a problem filling each cell here 
+      // todo: find cells neightbors that are alive
+      const neighborCount = findNeighborCount(grid, i, j);
+      console.log(neighborCount)
+    }
 
-// function setup() {
-//   grid = fillArr(cols, rows)
-//   for (let i = 0; i < cols; i++) {
-//     for (let j = 0; j < rows; j++) {
-//       grid[i][j] = grid.fill(math.floor(Math.random(2)))
-      
-//     }
-//   }
-//   return grid
-// }
+  } 
+}
+
+const cell = iterateGrid()
+
+//find count of neighbors
+function findNeighborCount(grid, i, j) {
+  let aliveNeightborCount = 0;
+  /**
+   *  NW  N.  NE
+   *  W  Cell E
+   *      S
+   * 
+   * i => up and down
+   * j => left and right
+   */
+   let neighbors = {
+    NW: grid[i - 1][j - 1],
+    N: grid[i - 1][j],
+    NE:grid[i - 1][j + 1],
+    W: grid[i][j - 1],
+    E: grid[i][j + 1],
+    SW: grid[i + 1][j - 1],
+    S: grid[i + 1][j],
+    SE: grid[i + 1][j + 1],
+  }
+        
+  for (const coordinate in neighbors) {
+    if (neighbors.hasOwnProperty(coordinate)) {
+      // console.log(`coordinates ${coordinate} neighbor ${neighbors[coordinate]}`)
+      const neighbor = neighbors[coordinate];
+      if (neighbor) aliveNeightborCount += 1;
+    }
+  }
+  return aliveNeightborCount;
+}
+
